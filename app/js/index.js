f4ce6b2c46b2a0b90ce8bb72ff42f081ff364518
Kostyshyn <kostyshyn@ua.fm>
2018-06-06 17:41:21 +0300

    [+] add dependencies, pages

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,32 @@
-$(() => {
-	console.log(1)
-});
+// $('.products-carousel').slick({
+//     infinite: true,
+//     slidesToShow: 6,
+//     slidesToScroll: 3,
+//     vertical: true,
+//     autoplay: true,
+//     autoplaySpeed: 1000,
+//     responsive: [
+//     {
+//         breakpoint: 1024,
+//         settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 3,
+//             infinite: true,
+//             // dots: true
+//         }
+//     },
+//     {
+//         breakpoint: 600,
+//         settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 2
+//         }
+//     },
+//     {
+//         breakpoint: 480,
+//         settings: {
+//             slidesToShow: 1,
+//             slidesToScroll: 1
+//         }
+//     }]
+// });
