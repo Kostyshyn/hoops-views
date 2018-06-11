4f57af0cc74d895142a2803b65bcc8e0b05a0c9f
Kostyshyn <kostyshyn@ua.fm>
2018-06-11 17:35:44 +0300

    [+] shopping cart modal

--- app/js/index.js
+++ app/js/index.js
@@ -1,40 +1,40 @@
-const verticalGallery = () => ( window.innerWidth > 991 );
-
-$('.preview-gallery').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    vertical: verticalGallery(),
-    // autoplay: true,
-    // autoplaySpeed: 1000,
-	prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-up"></i></button>',
-	nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-down"></i></button>',
-    responsive: [
-    {
-        breakpoint: 1024,
-        settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2
-            // dots: true
-        }
-    },
-    {
-        breakpoint: 600,
-        settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-        }
-    },
-    {
-        breakpoint: 480,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-        }
-    }]
-});
-
 $(() => {
+    const verticalGallery = () => ( window.innerWidth > 991 );
+
+    $('.preview-gallery').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        vertical: verticalGallery(),
+        // autoplay: true,
+        // autoplaySpeed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-up"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-down"></i></button>',
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2
+                // dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+
 	$('.slider').slick({
 	    slidesToShow: 1,        
 	    autoplay: true,
@@ -55,5 +55,10 @@ $(() => {
 	$('.color-select li input').click((e) => {
 		$('.color-selected span').text(e.target.value);
 	});
+
+    $('.search-toggle').click((e) => {
+        $('.logo').toggle();
+        $('.search-input').toggleClass('hide');
+    });
 	
 });
